da568775866805a81c36d1436521e60af46574dc
유성(yuseong) <62369592+yuseong01@users.noreply.github.com>
2025-03-27 22:29:41 +0900

    [ADD] 빗방울 크기별 색, 점수설정

--- Assets/Scripts/Rain.cs
+++ Assets/Scripts/Rain.cs
@@ -4,13 +4,50 @@ using UnityEngine;
 
 public class Rain : MonoBehaviour
 {
+    float size = 1.0f;
+    int score = 1;
+
+    //색깔설정은 SpriteRender에 있어서 이를 가져오기 위함 
+    SpriteRenderer renderer;
+
     // Start is called before the first frame update
     void Start()
     {
+        //Sprite Renderer컴포넌트를 GetComponent를 통해 가져온다
+        //스크립트와 같은 자리에 있어야함
+        renderer = GetComponent<SpriteRenderer>();
+
         float x = Random.Range(-2.4f, 2.4f);
         float y = Random.Range(3.0f, 5.0f);
 
         transform.position = new Vector3(x, y, 0);
+
+        //정수값을 넣어주면 정수만 추출됨(최댓값은 포함이 되지않음)
+        int type = Random.Range(1, 4);
+
+        if(type==1)
+        {
+            size = 0.8f;
+            score = 1;
+            //Color값은 float값이 들어감
+            //150을 넣고 싶다면 150/255f 값을 넣어야함
+            renderer.color = new Color(100 / 255f, 100 / 255f, 1f, 1f);
+        }
+        else if (type == 2)
+        {
+            size = 1.0f;
+            score = 2;
+            renderer.color = new Color(130 / 255f, 130 / 255f, 1f, 1f);
+        }
+        else if (type == 3)
+        {
+            size = 1.2f;
+            score = 3;
+            renderer.color = new Color(150 / 255f, 150 / 255f, 1f, 1f);
+
+        }
+
+        transform.localScale = new Vector3(size, size, 0);
     }
 
     // Update is called once per frame
