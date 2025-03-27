39a6084d6a706866705a9b8b155ad647c9647de9
유성(yuseong) <62369592+yuseong01@users.noreply.github.com>
2025-03-27 19:49:15 +0900

    [FIX] name을 Tame비교로 변경

--- Assets/Scripts/Rain.cs
+++ Assets/Scripts/Rain.cs
@@ -21,7 +21,7 @@ public class Rain : MonoBehaviour
     {
         //collision에는 충돌하는 물체의 정보가 들어있다.
         //즉, Rain이 Ground에 부딪히면 부딪힌 Ground 관련된 게임 오브젝트 정보들이 들어있다
-        if(collision.gameObject.name=="Ground")
+        if(collision.gameObject.CompareTag("Ground"))
         {
             //gameObject는 Rain스크립트가 붙어있는 오브젝트(Rain)자체를 가리킴
             Destroy(gameObject);
